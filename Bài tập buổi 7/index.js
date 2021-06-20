b105111906f76678cc701ae4a1bd6e3027e86c88
tuantran12 <68801644+tuantran12@users.noreply.github.com>
2021-06-21 01:02:04 +0700

    Add files via upload

---
+++ Bài tập buổi 7/index.js
@@ -0,0 +1,75 @@
+// B1
+// const container = document.getElementById(`list`)
+// for (i = 0; i<3; i++) {
+//     container.insertAdjacentHTML("beforeend", `<li>${i}</li>`)
+// }
+
+// B2
+// var sayhello = setInterval(hello1, 3000);
+// function hello1() {
+//     alert('hello world')
+// }
+
+// setTimeout(function(){
+//     console.log(`hi nick`)
+// },5000)
+
+// B4
+// var xyz = document.getElementsByTagName('li')
+// console.log(xyz[1])
+// for (i = 0; i<xyz.length; i++){
+//     console.log(xyz[i])
+// }
+
+// B5
+// var kol = document.getElementsByTagName('div')
+// console.log(kol[2]);
+// for (i=0; i<kol.length; i++) {
+//     console.log(kol[i])
+// }
+
+// B6 remove
+// var mas = document.getElementsByTagName('div')
+// var dsa = document.getElementById(`div-02`)
+// dsa.remove()
+// console.log(mas)
+// console.log(dsa)
+
+// B7
+// ---- Cách Update
+// window.onload = function ( ){
+//     document.querySelector("#button1")
+//         .addEventListener("click", (e) => {
+//             console.log(e.target)
+//     })
+// } 
+
+// window.onload = function ( ){
+//     document.querySelector("#inp1")
+//         .addEventListener("keydown", (e) => {
+//             console.log(e.key)
+//     })
+// } 
+
+// B8 - B9
+// name = prompt ('dien vao')
+// whis = prompt ('dien vao')
+// function hi (name,whis) {
+//     alert(`hi ${name} năm nay tôi muốn ${whis}`)
+// }
+// hi(name,whis)
+
+// B10
+// x = prompt ('dien vao')
+// i = prompt ('dien vao')
+// var users = {
+//     name: x,
+//     wish: i,
+// }
+// let a = users.name;
+// console.log(a);
+// let y = users.wish;
+// function myFunction6(a) {
+//     alert(a);
+// }
+// myFunction6('ankeo')
